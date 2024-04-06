8b8f911004e327be6ff1da27b6a477c6d7a0b7e4
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2024-04-06 22:07:05 +0700

    reorganize file

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ function setWebhook() {
 
 function doPost(e) {
   try {
-    var data = JSON.parse(e.postData.contents);
-    handle(data);
+    const data = JSON.parse(e.postData.contents);
+    switch (getCommand_(data.message.text)) {
+      case "about":
+        aboutHandler(data.message);
+        break;
+      case "showbalance":
+        showBalanceHandler(data.message);
+        break;
+      case "newbilling":
+        newBillingHandler(data.message);
+        break;
+      case "join":
+        joinHandler(data.message);
+        break;
+      case "editbalance":
+        editBalanceHandler(data.message);
+        break;
+      case "addmember":
+        addMemberHandler(data.message);
+        break;
+      case "removemember":
+        removeMemberHandler(data.message);
+        break;
+      default:
+    }
   } catch (err) {
     console.log(err);
     sendMessage(data.message.chat.id, `Error: ${err}`);
@@ -15,7 +38,7 @@ function doPost(e) {
 }
 
 function debug() {
-  var data = {
+  const data = {
     message: {
       chat: {
         id: -256622337,
